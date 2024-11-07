911a5e48cea33b51e6547a79762097bf8f4f343d
LarryG <larryg@klassitech.co.uk>
2024-11-07 15:05:39 +0000

    chore(): updating code

--- src/ocr.js
+++ src/ocr.js
@@ -1,11 +1,11 @@
 require ('dotenv').config();
 const { createWorker } = require('tesseract.js');
-const astellen = require('klassijs-astellen');
+const { astellen} = require('klassijs-astellen');
 
 const envName = env.envName.toLowerCase();
-const browserName = astellen.get('BROWSER_NAME');
 
 async function ocrGetText(visualBaseline) {
+const browserName = astellen.get('BROWSER_NAME');
   let worker = await createWorker();
   const imagePath = 'artifacts/visual-regression/original/' + browserName + '/' + envName + '/positive/';
   const {
